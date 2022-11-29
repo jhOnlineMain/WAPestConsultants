7336bd42e93914db369917dda87c48aa5cd0bb34
jhdev611 <62648319+jhOnlineMain@users.noreply.github.com>
2022-11-29 11:54:25 +0800

    build

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,6 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import {Helmet} from "react-helmet";
 import Header from "./header"
 import "./layout.scss"
 import { StaticImage } from "gatsby-plugin-image";
