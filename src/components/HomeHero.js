08905cf8aea6277b2904d5957fc1efd0744e6e8d
Emmett Gerard Hoolahan <emmetthoolahan@gmail.com>
2022-11-29 18:17:20 +0800

    css

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -9,7 +9,7 @@ import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const HomeHero = () => (
-  <div className="mx-auto max-w-7xl w-full pt-16 pb-0 text-center lg:py-48 lg:text-left">
+  <div className="mx-auto overflow-hidden max-w-7xl w-full pt-16 pb-0 text-center lg:py-48 lg:text-left">
             <div className="px-4 pt-6 pb-4 md:pt-0 lg:w-1/2 sm:px-8 xl:pr-16">
               <h1 className="text-5xl md:text-4xl tracking-tight font-extrabold flex flex-col text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
                 <span className="block xl:inline">Perth's Best</span>{' '}
